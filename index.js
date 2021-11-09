3602200c248758b6d06b8891b30c3cd4398c5724
Oleksii <alexmatvichuk@gmail.com>
2021-11-09 20:54:41 +0100

    add functions - contacts.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// console.log("__filename", __filename);
+// console.log("__dirname", __dirname);
+// console.log("process.argv", process.argv);
+// console.log("process.cwd()", process.cwd());
 
+const { contactsPath } = require("./contacts");
+
+console.log(contactsPath);
