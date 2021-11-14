0882e488318b61a23bb64e935042ef1cddbec222
Oleksii <alexmatvichuk@gmail.com>
2021-11-14 12:43:51 +0100

    add functions

--- contacts.js
+++ contacts.js
@@ -1,24 +1,44 @@
+const { uuid } = require("uuidv4");
+
+const fs = require("fs/promises");
 const path = require("path");
-const fs = require("fs").promises;
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-function listContacts() {
-  // ...твой код
-}
-
-function getContactById(contactId) {
-  // ...твой код
-}
-
-function removeContact(contactId) {
-  // ...твой код
-}
-
-function addContact(name, email, phone) {
-  // ...твой код
-}
-
-module.exports = {
-  contactsPath,
+const listContacts = async () => {
+  const contactsData = await fs.readFile(contactsPath);
+  const normalizeData = JSON.parse(contactsData);
+  // console.log(normalizeData);
+  return normalizeData;
 };
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const findContacts = contacts.find((contacts) => contacts.id === contactId);
+  // console.log("findContacts+++++++++", findContacts);
+  return findContacts;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  const updatedData = contacts.filter((_, index) => index !== idx);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedData));
+  return contacts[idx];
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const randomId = uuid().slice(0, 5);
+  const newContacts = {
+    id: randomId,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContacts);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContacts;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-// console.log("__filename", __filename);
-// console.log("__dirname", __dirname);
-// console.log("process.argv", process.argv);
-// console.log("process.cwd()", process.cwd());
+const getData = require("./contacts");
 
-const { contactsPath } = require("./contacts");
+// console.log("__filename:::::", __filename);
+// console.log("__dirname:::::", __dirname);
+// console.log("process.argv:::::", process.argv);
+// console.log("process.cwd():::::", process.cwd());
 
-console.log(contactsPath);
+// console.log("contactsPath:::::", contacts.contactsPath);
+// console.log("listContacts:::::", listContacts());
+// console.log("getContactById:::::", contacts.getContactById(4));
+// console.log("removeContact:::::", contacts.removeContact());
+// getData.listContacts();
+// getData.getContactById(2);
+
+const test = async (num) => {
+  return getData.addContact("Alex", "lolikdent@gmail.com", 444565446);
+};
+
+test();
